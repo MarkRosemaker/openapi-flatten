274edd49185dc05c9aad00b60b3d79a8186e0956
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-04-07 22:35:13 +0200

    clean up schema name

--- schema_refs.go
+++ schema_refs.go
@@ -2,6 +2,7 @@ package flatten
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/MarkRosemaker/errpath"
 	"github.com/MarkRosemaker/openapi"
@@ -11,7 +12,7 @@ import (
 func schemaRefs(d *openapi.Document, ss openapi.SchemaRefs, prefix string) error {
 	for name, s := range ss.ByIndex() {
 		if err := schemaRef(d, s,
-			strcase.ToGoPascal(fmt.Sprintf("%s %s", prefix, name)), moveIfNecessary); err != nil {
+			strcase.ToGoPascal(fmt.Sprintf("%s %s", prefix, strings.ReplaceAll(name, "/", " "))), moveIfNecessary); err != nil {
 			return &errpath.ErrKey{Key: name, Err: err}
 		}
 	}
