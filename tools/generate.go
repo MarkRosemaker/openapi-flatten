807c63703e2609744ce4300f69962356d2bf6ea6
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-05-01 17:19:45 +0200

    generate golden files

---
+++ tools/generate.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/MarkRosemaker/openapi"
+	flatten "github.com/MarkRosemaker/openapi-flatten"
+)
+
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(ctx context.Context) error {
+	entries, err := os.ReadDir("testdata")
+	if err != nil {
+		return err
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			continue
+		}
+
+		doc, err := openapi.LoadFromFile(filepath.Join("testdata", entry.Name(), "openapi.json"))
+		if err != nil {
+			return err
+		}
+
+		if err := flatten.Document(doc); err != nil {
+			return err
+		}
+
+		if err := writeJSON(filepath.Join("testdata", entry.Name(), "golden.json"), doc); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func writeJSON(path string, doc *openapi.Document) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if err := doc.WriteJSON(f); err != nil {
+		return fmt.Errorf("writing to %q: %w", path, err)
+	}
+
+	return nil
+}
